b100558a7f967573e7b90447164e66fe42946a9c
mohitzalavadiya <mzpatel14@gmail.com>
2022-11-24 12:42:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:emicalculator/emi.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: EmiCalculator(),
+    ),
+  );
+}
